23cf9c46cbd340b1545acba1f0d3ec5d1adebcfe
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-09-13 21:16:51 +0300

    Add files via upload

---
+++ module_13_3_SupportBot.py
@@ -0,0 +1,24 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = '7318149436:AAGsSUVCDgZhtmeaAEPHaoSxhZDaYpYoO_U'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())  # Added parentheses to initialize MemoryStorage
+
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    print('Введена команда /start')
+
+
+@dp.message_handler()
+async def all_massages(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    print('У вас новое сообщение')
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
